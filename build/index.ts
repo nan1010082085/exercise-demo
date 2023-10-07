594545fa08058808477a3338b4caae86b4e118ff
yangdongnan <nan1010082085@163.com>
2023-10-07 13:07:55 +0800

    feat(other): 🎸 增加生成静态版本文件v.json

---
+++ build/index.ts
@@ -0,0 +1,60 @@
+import * as path from 'node:path';
+import * as fs from 'node:fs';
+import dayjs from 'dayjs';
+import { red, green, blue } from 'kolorist';
+
+interface IGetPackage {
+  name: string;
+  version: string;
+  dependencies: Record<string, string>;
+  devDependencies: Record<string, string>;
+  time: string;
+}
+
+// 获取package文件
+function getPackage(): null | IGetPackage | undefined {
+  const packPath = path.join(__dirname, '../package.json');
+  if (!fs.existsSync(packPath)) {
+    console.log(red('文件不存在'));
+    return;
+  }
+  const packageObject = fs.readFileSync(packPath);
+  if (Buffer.isBuffer(packageObject)) {
+    const bufferStr = packageObject.toString();
+    const { name, version, dependencies, devDependencies } = JSON.parse(bufferStr);
+    const time = dayjs().format('YYYY-MM-DD HH:mm:ss');
+    return {
+      name,
+      version,
+      dependencies,
+      devDependencies,
+      time
+    };
+  }
+  return null;
+}
+
+async function writePublic(obj: IGetPackage) {
+  const pathJson = path.join(__dirname, '../public/v.json');
+  const write = () => {
+    const jsonStr = JSON.stringify(obj, null, 2);
+    fs.writeFile(pathJson, jsonStr, (err) => {
+      if (err) {
+        console.log(red('写入版本JSON失败'));
+      }
+      console.log(blue(`当前时间 ${obj.time}`) + ' ' + green('写入版本JSON成功'));
+    });
+  };
+
+  if (fs.existsSync(pathJson)) {
+    write();
+  } else {
+    write();
+  }
+}
+
+const jsonObject = getPackage();
+
+if (jsonObject) {
+  writePublic(jsonObject);
+}
