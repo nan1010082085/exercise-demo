53f81a5fd3188db18c347d175f7ef09ca77d2e5f
yangdongnan <nan1010082085@163.com>
2023-12-01 16:17:06 +0800

    perf(views): ⚡️ 优化部件列表的数据结构和拖拽传递数据

--- src/constants/widget.models.ts
+++ src/constants/widget.models.ts
@@ -3,7 +3,7 @@ import type { BaseGeneralModels, MetadataModels } from './base.models';
 export const widgetDefualt = {
   id: '',
   name: '测试',
-  icon: 'https://tdesign.gtimg.com/demo/demo-image-1.png',
+  type: 'base-line',
   general: {
     position: {
       x: 0,
@@ -13,21 +13,40 @@ export const widgetDefualt = {
     }
   },
   metadata: {},
-  manifest: []
+  manifest: {
+    icon: 'https://tdesign.gtimg.com/demo/demo-image-1.png',
+    name: '',
+    fields: []
+  }
 };
 
-export interface ManifestModels {
+export interface WidgetConfigOpiton {
+  [k: string]: {
+    title: string;
+    children: string[];
+  };
+}
+
+export interface FieldModels {
   label: string;
-  type: string;
+  value: string;
   description: string;
+}
+
+export interface ManifestModels {
+  id: string;
+  name: string;
+  type: string;
+  icon: string;
+  fields: FieldModels[];
 }
 
 // 部件数据
 export interface WidgetModels {
   id: string;
   name: string;
-  icon: string;
+  type: string;
   general: BaseGeneralModels;
   metadata: MetadataModels;
-  manifest: Partial<ManifestModels[]>;
+  manifest: Partial<ManifestModels>;
 }
--- src/views/dashboard/editor/widget-drawer/index.tsx
+++ src/views/dashboard/editor/widget-drawer/index.tsx
@@ -1,43 +1,82 @@
-import { defineComponent, inject, ref, watch } from 'vue';
+import { computed, defineComponent, inject, onMounted, ref, watch } from 'vue';
 import styles from './index.module.scss';
 import { DrawerTypeKey } from '../inject.key';
-import { widgetDefualt, type WidgetModels } from '@/constants/widget.models';
-import { Image } from 'tdesign-vue-next';
-
-const defaultData = [widgetDefualt, widgetDefualt, widgetDefualt];
+import { type ManifestModels } from '@/constants/widget.models';
+import { Collapse, CollapsePanel, Image as TImage } from 'tdesign-vue-next';
+import widgetConfig from '@widget/lib/config';
+import { mainfestInstall } from '@/widget/lib/manifest';
+const dargSrc = new URL('@public/vite.svg', import.meta.url).href;
 
 const WidgetDrawer = defineComponent({
   name: 'WidgetDrawer',
   setup() {
-    const widgets = ref<WidgetModels[]>(defaultData);
+    const widgetsConfig = computed<string[]>(() => Object.keys(widgetConfig));
     const drawer = inject(DrawerTypeKey);
+    const defaultValue = ref([0]);
+    const img = ref();
 
-    const onDragStart = (e: DragEvent, widget: WidgetModels) => {
-      console.log('drag start', widget);
-      e.dataTransfer?.setData('widget', JSON.stringify(widget));
-      const dragImage = document.createElement('div');
-      dragImage.classList.add('drag-image');
-      e.dataTransfer?.setDragImage(dragImage, 10, 10);
+    const reserveCreateImage = () => {
+      img.value = document.createElement('img');
+      let canvas = document.createElement('canvas');
+      const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+      let linearGradient = ctx.createLinearGradient(0, 0, 50, 50);
+      linearGradient.addColorStop(0, '#fff');
+      linearGradient.addColorStop(0.5, '#fff');
+      linearGradient.addColorStop(1, '#eee');
+      ctx['fillStyle'] = linearGradient;
+      ctx['lineJoin'] = 'round';
+      ctx['strokeStyle'] = linearGradient;
+      ctx?.fillRect(0, 0, 50, 50);
+      ctx?.beginPath();
+      ctx?.lineTo(0, 0);
+      ctx?.lineTo(50, 0);
+      ctx?.lineTo(0, 0);
+      ctx?.lineTo(0, 50);
+      ctx?.closePath();
+      ctx?.stroke();
+      img.value.src = canvas.toDataURL('image/jpg');
     };
+
+    const onDragStart = (e: DragEvent, manifest: ManifestModels) => {
+      console.log('drag start', manifest);
+      e.dataTransfer?.setData('widget', JSON.stringify(manifest));
+      e.dataTransfer?.setDragImage(img.value, 1, 1);
+    };
+
+    onMounted(() => {
+      reserveCreateImage();
+    });
 
     return () => {
       return (
         <div class={[styles['widget-drawer'], drawer?.value.widget ? styles.visible : '']}>
-          {widgets.value.map((widget) => {
-            return (
-              <div
-                draggable={true}
-                onDragstart={(e: DragEvent) => onDragStart(e, widget)}
-                onDragover={(e) => e.preventDefault()}
-                class={styles['widget-card']}
-              >
-                <div class={styles['widget-card-icon']}>
-                  <Image fit={'cover'} src={widget.icon} />
-                </div>
-                <div class={styles['widget-card-name']}>{widget.name}</div>
-              </div>
-            );
-          })}
+          <Collapse defaultValue={defaultValue.value}>
+            {widgetsConfig.value.map((widgetConfigKey) => {
+              const widgetKey = widgetConfig[widgetConfigKey];
+              const widgtKeys = widgetKey.children;
+              return (
+                <CollapsePanel class={styles['widget-panel']} header={widgetKey.title}>
+                  {widgtKeys.map((type: string) => {
+                    const manifest = mainfestInstall(type);
+                    return (
+                      <div
+                        key={type}
+                        draggable={true}
+                        onDragstart={(e: DragEvent) => onDragStart(e, manifest)}
+                        onDragover={(e) => e.preventDefault()}
+                        class={styles['widget-card']}
+                      >
+                        <div class={styles['widget-card-icon']}>
+                          <TImage fit={'cover'} src={manifest.icon} />
+                        </div>
+                        <div class={styles['widget-card-name']}>{manifest.name}</div>
+                      </div>
+                    );
+                  })}
+                </CollapsePanel>
+              );
+            })}
+          </Collapse>
         </div>
       );
     };
