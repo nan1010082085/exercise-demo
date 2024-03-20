399844795fa5d6b8b2345dc7988e019f56b8fb32
yangdongnan <nan1010082085@163.com>
2024-03-20 09:29:39 +0800

    fix(test): 🐛 修复模块大写无法自动获取文件名

--- src/router/path/text-view.ts
+++ src/router/path/text-view.ts
@@ -4,14 +4,14 @@ const creategory = import.meta.glob('@/test-v/**');
 const routes = import.meta.glob('@/test-v/*-components/**/*.(tsx|vue)');
 
 const creategoryKey = Object.keys(creategory)
-  .map((key) => key.match(/[\w]*-components+/)?.[0])
+  .map((key) => key.match(/[\w\W]*-components+/)?.[0])
   .filter((f) => f);
 const creategoryRoutes = Array.from(new Set(creategoryKey)) as string[];
 
 
 const textViewRoutes = Object.keys(routes).map((key) => {
   const KeyArrs = key.split('/');
-  const creategory = key.match(/[a-z]+-components/g);
+  const creategory = key.match(/[a-zA-Z]+-components/g);
   const name = KeyArrs[KeyArrs.length - 1].split('.')[0];
   return {
     name,
@@ -22,6 +22,8 @@ const textViewRoutes = Object.keys(routes).map((key) => {
 });
 
 const TextView: RouteRecordRaw[] = textViewRoutes;
+
+console.log(TextView)
 
 export { creategoryRoutes, textViewRoutes };
 export default TextView;
